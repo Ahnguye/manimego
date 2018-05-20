95ab7692a27a4ce19271601ace1dc9d799188081
Andre Nguyen <ahnguye@ucdavis.edu>
2018-05-20 16:42:06 -0700

    tem

--- mani.me (copy).go
+++ mani.me (copy).go
@@ -28,13 +28,57 @@ func (router *MyRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path == "/add" {
         fmt.Fprintf(w, "Sending message to client through customized router")
 
+
+        buf := new(bytes.Buffer)
+        buf.ReadFrom(r.Body)
+        newStr := buf.String()
+
+        fmt.Printf(newStr)
+        //body, err := ioutil.ReadAll(r.Body)
+        //writeError(err)
+
+        //fmt.Printf("%s", body)
+        insertDatabase(router.db, newStr)
         return
     } else if r.URL.Path == "/get" {
-	fmt.Fprintf(w, "Sending message to client through customized router")
-         
+            queryDatabase(router.db, w, "SELECT * FROM orders;")
         return
     } 
     return
+}
+
+func queryDatabase(db *sql.DB, w http.ResponseWriter, query_str string) {
+    rows, err := db.Query(query_str)
+    writeError(err)
+
+    for rows.Next() {
+        var uid string
+        var data string
+
+        err = rows.Scan(&uid, &data)
+        writeError(err)
+
+        fmt.Fprintf(w, "%-10v %100v\n\n", uid, data)
+    }
+
+}
+
+func insertDatabase(db *sql.DB, str string) {
+
+    var lastInsertId int
+    // QueryRow returns a row.
+    //INSERT INTO orders (data) VALUES('{"name": "Paint house", "tags": ["Improvements", "Office"], "finished": false}');
+
+    err := db.QueryRow("INSERT INTO orders(data) VALUES($1) returning uid;", str).Scan(&lastInsertId)
+    writeError(err)
+    fmt.Printf("%d\n", lastInsertId)
+}
+
+func writeError(err error) {
+    if err != nil {
+        panic(err)
+    
+    }
 }
 
 func main() {
